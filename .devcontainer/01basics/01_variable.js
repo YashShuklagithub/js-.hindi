d0512be09e9fe10017bc51c1336b037191467c47
YashShuklagithub <yashshuklaji1155@gmail.com>
2024-05-28 17:07:56 +0000

    container config done

---
+++ .devcontainer/01basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountid=12234
+let accountemail="yash@gmail.com"
+var accpassword=1234
+let acccity="jabalpur"
+console.log(accountemail)
+console.table([accountid,accountemail,accpassword,acccity])
